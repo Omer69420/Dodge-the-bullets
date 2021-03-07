2f8267a87649354e84d32f8940a443b4206fbaaa
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2021-03-08 00:16:59 +0100

    boundaries för playern, v1 kan förbättras

--- player.lua
+++ player.lua
@@ -11,17 +11,33 @@ end
 
 function Player:update(dt)
     if love.keyboard.isDown("d") 
-    then self.x = self.x + self.speed * dt
-        end
+    then self.x = self.x + self.speed * dt --riktning höger
+    end
+    
     if love.keyboard.isDown("s")
-    then self.y = self.y + self.speed * dt
-        end
+    then self.y = self.y + self.speed * dt -- riktning ner
+    end
+    
     if love.keyboard.isDown("a")
-    then self.x = self.x - self.speed * dt
-        end
+    then self.x = self.x - self.speed * dt -- riktning vänster
+    end
+    
     if love.keyboard.isDown("w")
-    then self.y = self.y - self.speed * dt
-        end
+    then self.y = self.y - self.speed * dt -- riktning uppercase
+    end
+
+    if self.y < 0 
+    then self.y = 0
+    elseif self.y + self.height > love.graphics.getHeight() 
+    then self.y = love.graphics.getHeight() - self.height
+    end
+
+    if self.x < 0
+    then self.x = 0
+    elseif self.x + self.width > love.graphics.getWidth()
+    then self.x = love.graphics.getWidth() - self.width
+    end
+
 end
 
 function Player:move(dt)
