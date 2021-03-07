1053c12d8eb71b61ff7d2c142cecf8627d7aa7e2
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2021-03-07 22:42:19 +0100

    player specifikationer

--- player.lua
+++ player.lua
@@ -2,10 +2,11 @@
 Player = {}
 
 function Player:load()
-
-
-
-
+    self.x = love.graphics.getWidth() / 2   
+    self.y = love.graphics.getHeight() / 2
+    self.width = 50
+    self.height = 50
+    self.speed = 400
 end
 
 function Player:update(dt)
