7f8368aebe836de8d1d5db606ab2f9f34b68801c
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2021-03-08 00:02:35 +0100

    cirkeln rör på sig åt fyra olika hållen

--- player.lua
+++ player.lua
@@ -2,15 +2,26 @@
 Player = {}
 
 function Player:load()
-    self.x = love.graphics.getWidth() / 2   
-    self.y = love.graphics.getHeight() / 2
-    self.width = 50
-    self.height = 50
-    self.speed = 400
+    self.x = love.graphics.getWidth() / 2   --bredden av windows rutan delat på 2 för att få mitten.
+    self.y = love.graphics.getHeight() / 2  -- längden av windows rutan delat på 2 för att få mitten.
+    self.width = 25
+    self.height = 25
+    self.speed = 300
 end
 
 function Player:update(dt)
-
+    if love.keyboard.isDown("d") 
+    then self.x = self.x + self.speed * dt
+        end
+    if love.keyboard.isDown("s")
+    then self.y = self.y + self.speed * dt
+        end
+    if love.keyboard.isDown("a")
+    then self.x = self.x - self.speed * dt
+        end
+    if love.keyboard.isDown("w")
+    then self.y = self.y - self.speed * dt
+        end
 end
 
 function Player:move(dt)
