8a85c064f7862800625441d80e175ef2b8c9791a
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2021-03-29 15:49:16 +0200

    kommentarer i bullet filen

--- bullet.lua
+++ bullet.lua
@@ -1,19 +1,18 @@
-bullet = {}
+bullet = {}     -- definierar Bullet
 
-function bullet:load()
+function bullet:load() -- Bullets värden
     x = 10
     y = 150
     self.width = 40
     self.height = 20
 end
 
-function bullet:update(dt)
+function bullet:update(dt)  -- Bullets hastighet
     x = x + 100 * dt 
 end
 
 
-
 function bullet:draw()
-    love.graphics.circle("fill", x, y, self.height)
+    love.graphics.circle("fill", x, y, self.height)  -- Tillfällig cirkel för Bullets utseende
 end
 
