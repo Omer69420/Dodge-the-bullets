38bb5607be533b968468721563d1f9f0501da3e1
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2021-04-11 18:13:50 +0200

    nästan klar

--- Main.lua
+++ Main.lua
@@ -1,48 +1,41 @@
 ---@diagnostic disable: lowercase-global
-
-
+ 
+ 
 require("player")  --länkning till Player mappen.
 require("bullet")  --länkning till Bullet mappen.
-
+ 
+local isGameOver = false
+ 
 -- för att veta vad Player gör måste jag länka lua filerna på detta vis och samma med Bullet mappen.
-
---function checkCollision(Player, bullet)
-  --  if love.physics.getDistance(Player ,bullet) < 5 then 
-    --    return true
-    --end
---end
-
--- if bullet.x + bullet.width > player.x
--- and bullet.x < player.x + player.width
--- and bullet.y + bullet.height > player.y
--- and bullet.y < player.y + player.height
-
---local collided = checkCollision(Player.fixture, bullet.fixture)
-
---if collided then
-  --  print("Game over")
---end
-    
-
-
+     
 function love.load() -- laddar in filerna   
     Player:load()
     bullet:load()  
 end
-
+ 
 function love.update(dt) -- updaterar de(de rör på sig)
     Player:update(dt)
     bullet:update(dt)
-    checkCollision(Player, bullet)
+    if(checkCollision(Player, bullet)) then
+        isGameOver = true
+    end
 end
-
+ 
 function love.draw() -- ritar ut filernas info
+    
+    if(isGameOver) then
+        local font = love.graphics.newFont(40)
+        love.graphics.setFont(font)
+        local text = "GAME OVER!"
+        love.graphics.print(text, love.graphics.getWidth() / 2 - font:getWidth(text) / 2, love.graphics.getHeight() * (1 / 3))
+        return
+    end
+ 
     Player:draw()
     bullet:draw()
+ 
 end
-
-
-
+ 
 function checkCollision(a, b)
     if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
         return true
@@ -50,9 +43,11 @@ function checkCollision(a, b)
         return false
     end
 end
-
+ 
 function love.keypressed(key)  -- ifall man vill stänga av spelet
     if key == "escape" then
         love.event.quit()
+    elseif key == "r" then
+        love.event.quit("restart")
     end
 end
--- bullet.lua
+++ bullet.lua
@@ -1,24 +1,23 @@
 bullet = {}     -- definierar Bullet
 
 function bullet:load() -- Bullets värden
-    x  = love.math.random(710)
-    y = 0
+    self.x  = love.math.random(710)
+    self.y = 0
     self.width = 20
     self.height = 10
 end
-
+ 
 function bullet:update(dt)  -- Bullets hastighet
-    y = y + 250 * dt 
+    self.y = self.y + (250 * dt)
 end
-
+ 
 function bullet:collide()
     if checkCollision(self, Player) then
-        self.speed = 0 
+        self.speed = 0
     end
 end
-
-
+ 
 function bullet:draw()
     love.graphics.setColor(1,0,0)  -- färgen av bullets
-    love.graphics.circle("fill", x , y, self.height)  -- Tillfällig cirkel för Bullets utseende
+    love.graphics.circle("fill", self.x , self.y, self.height)  -- Tillfällig cirkel för Bullets utseende
 end
